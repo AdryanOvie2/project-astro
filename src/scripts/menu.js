c59a5e9267e0253f2a6c6d30c213d7d83289aebf
AdryanOvie2 <adryanovie2@gmail.com>
2023-06-25 15:02:42 +0200

    check Js menu commit

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,22 +1,9 @@
-const menUl = document.getElementById("menu");
-const hamburguerIcon = document.getElementById('hamburguerIcon');
-const closeIcon = document.getElementById("closeIcon");
+
 let isImg = true;
 // const interruptor = document.getElementById("darkMode");
 // const nav = document.getElementById("navbar");
 
-hamburguerIcon.addEventListener("click", function() {
-    menUl.classList.toggle("hidden");
-    console.log("click");
-    hamburguerIcon.classList.add("hidden");
-    closeIcon.classList.remove("hidden");
-});
 
-closeIcon.addEventListener("click", function() {
-    menUl.classList.toggle("hidden");
-    closeIcon.classList.add("hidden");
-    hamburguerIcon.classList.remove("hidden");
-});
 
 // dark mode
 // interruptor.addEventListener("click", function (){
